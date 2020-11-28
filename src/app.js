786a40a3588bafba25cbe86a4e243dd7d7d4812f
Audry-prog <audryavalos@hotmail.com>
2020-11-28 16:50:23 -0300

    correções

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/PetRide", { 
+mongoose.connect("mongodb://localhost:27017/petride", { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 });
