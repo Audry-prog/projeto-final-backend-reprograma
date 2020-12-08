56e0e6d5742085e529858ea5f06fe142f832a725
Audry-prog <audryavalos@hotmail.com>
2020-12-08 18:30:10 -0300

    testando metodos

--- src/controllers/motoristasController.js
+++ src/controllers/motoristasController.js
@@ -1,19 +1,19 @@
 const motoristas = require('../models/motoristas');
 
-const getAll = (req, res) => {
-	motoristas.find(function (err, motoristas) {
-		err ? res.status(424).send({ message: err.message }) : res.status(200).send(motoristas);
-	});
-};
-
-const getAllMotoristasByBairro = (req, res) => {
+/*const getAllMotoristasByBairro = (req, res) => {
 	const bairro = req.query.bairro;
 	if (bairro) {
 		const motoristasByBairro = motoristas.filter((motorista) => motorista.bairro.includes(bairro));
 		res.status(200).send(motoristasByBairro);
 	} else {
-		res.status(404).send('Motoristas sem Bairro cadastrado');
+		res.status(200).send("Não há motoristas com o bairro cadastrado.");
 	}
+};*/
+
+const getAll = (req, res) => {
+	motoristas.find(function (err, motoristas) {
+		err ? res.status(424).send({ message: err.message }) : res.status(200).send(motoristas);
+	});
 };
 
 const postMotorista = (req, res) => {
@@ -69,8 +69,8 @@ const putMotorista = (req, res) => {
 };
 
 module.exports = {
-	getAll,
-	getAllMotoristasByBairro,
+    /*getAllMotoristasByBairro,*/
+    getAll,
 	postMotorista,
 	getById,
 	deleteMotorista,
