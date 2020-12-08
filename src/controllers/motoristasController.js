3f816bc7fffbc837ddc2b777fdb8af83a48d49df
Audry-prog <audryavalos@hotmail.com>
2020-12-08 17:16:02 -0300

    corrigi comentários do código getAllMotoristasByBairro

--- src/controllers/motoristasController.js
+++ src/controllers/motoristasController.js
@@ -1,76 +1,78 @@
-const motoristas = require('../models/motoristas')
+const motoristas = require('../models/motoristas');
 
 const getAll = (req, res) => {
-    motoristas.find(function (err, motoristas) {
-    err ? res.status(424).send({ message: err.message }) : res.status(200).send(motoristas);
-    })
+	motoristas.find(function (err, motoristas) {
+		err ? res.status(424).send({ message: err.message }) : res.status(200).send(motoristas);
+	});
 };
 
 const getAllMotoristasByBairro = (req, res) => {
-    const bairro = req.query.bairro 
-        if (bairro) { 
-            const motoristasByBairro = motoristas.filter(motorista => motorista.bairro.includes(bairro)) // encontro todos os filmes do gênero que filtrei
-        res.status(200).send(motoristasByBairro) 
-      } else { 
-        res.status(404).send('Bairro não encontrado'); 
-    }
-}
+	const bairro = req.query.bairro;
+	if (bairro) {
+		const motoristasByBairro = motoristas.filter((motorista) => motorista.bairro.includes(bairro));
+		res.status(200).send(motoristasByBairro);
+	} else {
+		res.status(404).send('Bairro não encontrado');
+	}
+};
 
 const postMotorista = (req, res) => {
-    let motorista = new motoristas(req.body);
-    motorista.save(err => {
-        err ? res.status(424).send({ message: err.message })
-        
-        : res.status(201).send({
-            status: true,
-        message: 'Novo Motorista cadastrado com sucesso'
-    });
-});
+	let motorista = new motoristas(req.body);
+	motorista.save((err) => {
+		err
+			? res.status(424).send({ message: err.message })
+			: res.status(201).send({
+					status: true,
+					message: 'Novo Motorista cadastrado com sucesso',
+			});
+	});
 };
 
 const getById = (req, res) => {
-    const id = req.params.id;
-    motoristas.find({ id }, (err, motoristas) => {
-        if(err) {
-            res.status(424).send({ message: err.message })
-        } else {
-           res.status(200).send(motoristas)  
-        }
-    })
+	const id = req.params.id;
+	motoristas.find({ id }, (err, motoristas) => {
+		if (err) {
+			res.status(424).send({ message: err.message });
+		} else {
+			res.status(200).send(motoristas);
+		}
+	});
 };
 
 const deleteMotorista = (req, res) => {
-    const id = req.params.id;
-    motoristas.find({ id }, (err, motorista) => {
-        if (motorista.length > 0) {
-            motoristas.deleteOne({ id }, err => {
-                err ? res.status(424).send({ message: err.message }) : res.status(200).send({
-                    status: true,
-                    mensagem: 'Motorista excluído com sucesso'
-                });
-            });
-        } else {
-            res.status(404).send('Motorista não encontrado');
-        };
-    });
+	const id = req.params.id;
+	motoristas.find({ id }, (err, motorista) => {
+		if (motorista.length > 0) {
+			motoristas.deleteOne({ id }, (err) => {
+				err
+					? res.status(424).send({ message: err.message })
+					: res.status(200).send({
+							status: true,
+							mensagem: 'Motorista excluído com sucesso',
+					});
+			});
+		} else {
+			res.status(404).send('Motorista não encontrado');
+		}
+	});
 };
 
 const putMotorista = (req, res) => {
-    const id = req.params.id;
-    motoristas.updateMany({ id }, { $set: req.body }, function(err, motoristas) {
-        if(err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(200).send({ message: "Motorista atualizado com sucesso!"})
-        }
-    })
+	const id = req.params.id;
+	motoristas.updateMany({ id }, { $set: req.body }, function (err, motoristas) {
+		if (err) {
+			res.status(500).send({ message: err.message });
+		} else {
+			res.status(200).send({ message: 'Motorista atualizado com sucesso!' });
+		}
+	});
 };
 
 module.exports = {
-    getAll,
-    getAllMotoristasByBairro,
-    postMotorista,
-    getById,
-    deleteMotorista,    
-    putMotorista
+	getAll,
+	getAllMotoristasByBairro,
+	postMotorista,
+	getById,
+	deleteMotorista,
+	putMotorista,
 };
--- src/models/motoristas.js
+++ src/models/motoristas.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-//Estrutura do seu model (atributos da sua entidade)
 const motoristasSchema = new mongoose.Schema({
     id: { type: Number },
     nome: { type: String },
@@ -14,13 +13,9 @@ const motoristasSchema = new mongoose.Schema({
     estado: { type: String },
     despesaTrajeto: { type: String }
 },{
-    //gera por padrão uma versão para cada atualização do documento
     versionKey: false
 });
 
-//atribuindo o esquema a uma collection
-//estou definindo o nome da collection que irei salvar no banco
 const motoristas = mongoose.model('motoristas', motoristasSchema);
 
-//exportar o model para ser utilizado
 module.exports = motoristas;
