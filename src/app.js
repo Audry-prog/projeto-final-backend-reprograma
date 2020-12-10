cc1bce2b397e55fb0240c826ebf512c37d85f315
Audry-prog <audryavalos@hotmail.com>
2020-12-10 15:20:48 -0300

    corrigindo bug do script

--- src/app.js
+++ src/app.js
@@ -1,40 +1,37 @@
-require ('dotenv-safe').config();
+require('dotenv-safe').config();
 
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect(`${process.env.MONGODB_URL}`, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 let db = mongoose.connection;
-db.on("error", console.log.bind(console, "connection error:"))
-db.once("open", function(){
-  console.log("Conexão realizada com sucesso.")
+db.on('error', console.log.bind(console, 'connection error:'));
+db.once('open', function () {
+	console.log('Conexão realizada com sucesso.');
 });
 
 //rotas
-const index = require("./routes/index");
-const motoristas = require("./routes/motoristasRoute");
-const tutores = require("./routes/tutoresRoute");
+const index = require('./routes/index');
+const motoristas = require('./routes/motoristasRoute');
+const tutores = require('./routes/tutoresRoute');
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
-  });
-  
-app.use("/", index);
-app.use("/motoristas", motoristas);
-app.use("/tutores", tutores);
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+app.use('/', index);
+app.use('/motoristas', motoristas);
+app.use('/tutores', tutores);
 
 module.exports = app;
