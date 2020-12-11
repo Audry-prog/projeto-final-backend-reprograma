1ab9b49e4a947241359b90877c00c4bd1aeec831
Audry-prog <audryavalos@hotmail.com>
2020-12-10 23:19:51 -0300

    ajuste rota getByName

--- src/routes/motoristasRoute.js
+++ src/routes/motoristasRoute.js
@@ -8,7 +8,7 @@ router.get("/horarios", controller.getMotoristasByHorario,)
 router.get("/ativos", controller.getMotoristasAtivos);
 router.get("/motoristas", controller.getAll);
 router.get("/:id", controller.getById);
-router.get("/:nome", controller.getByName);
+router.get("/nome/:nome", controller.getByName);
 router.post("/", controller.postMotorista);
 router.delete("/:id", controller.deleteMotorista);
 router.put("/:id", controller.putMotorista);
