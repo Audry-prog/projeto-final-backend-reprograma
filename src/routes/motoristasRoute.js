3d34d437edb8fc86621354150b58d01996cd6a09
Audry-prog <audryavalos@hotmail.com>
2020-12-10 22:38:03 -0300

    corrigi a rota do método getByName

--- src/routes/motoristasRoute.js
+++ src/routes/motoristasRoute.js
@@ -9,7 +9,7 @@ router.get("/ativos", controller.getMotoristasAtivos);
 router.get("/motoristas", controller.getAll);
 router.post("/", controller.postMotorista);
 router.get("/:id", controller.getById);
-router.get('/:nome', controller.getByName);
+router.get('/nome/:nome', controller.getByName);
 router.delete("/:id", controller.deleteMotorista);
 router.put("/:id", controller.putMotorista);
 
